5f7e9a445912f17475efcf33497ac0a51e4a53d1
LuisitooDev <laha11590@hotmail.com>
2024-04-21 20:42:49 -0700

    first commit

--- index.js
+++ index.js
@@ -1,45 +1,64 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors'); // Importa el paquete cors
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const mongoURI = 'mongodb+srv://luisitodev:dp8OiCTU9AfSi2bx@cluster0.9jkmrph.mongodb.net/'; // Reemplaza con tus propias credenciales y URL de conexión
-
 app.use(express.json());
-app.use(cors()); // Utiliza el middleware de CORS
+app.use(cors());
 
-async function connectToMongoDB() {
-    try {
-        const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-        console.log('Conectado a MongoDB');
-        return client.db();
-    } catch (error) {
-        console.error('Error al conectar a MongoDB:', error);
-    }
+const uri = "mongodb+srv://luisitodev:dp8OiCTU9AfSi2bx@cluster0.9jkmrph.mongodb.net";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Conectado a MongoDB");
+
+    const db = client.db(); // Obtiene la instancia de la base de datos
+
+    // Configura las rutas de Express después de que el cliente de MongoDB se haya conectado
+    app.get("/sensor-data", async (req, res) => {
+      try {
+        const collection = db.collection("sensorData");
+        const results = await collection.find({}).limit(50).toArray();
+        res.status(200).send(results);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+        res.status(500).send("Error interno del servidor");
+      }
+    });
+
+    // Add a new document to the collection
+    app.post("/insert-sensor-data", async (req, res) => {
+        try {
+            const collection = db.collection("sensorData");
+            const newDocument = req.body;
+            newDocument.date = new Date();
+            await collection.insertOne(newDocument);
+            res.sendStatus(204); // Envía un código de estado 204 (No Content)
+        } catch (error) {
+            console.error("Error al insertar datos:", error);
+            res.status(500).send("Error interno del servidor");
+        }
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    });
+
+  } finally {
+    // El bloque finally asegura que el cliente se cierre correctamente
+    // incluso si se produce un error al conectarse
+    //await client.close();
+  }
 }
 
-app.get('/', (req, res) => {
-    res.send('API funcionando');
-});
-
-// Endpoint para insertar datos en MongoDB
-app.post('/insert-sensor-data', async (req, res) => {
-    const db = await connectToMongoDB();
-    const sensorData = req.body;
-    const result = await db.collection('sensorData').insertOne(sensorData);
-    res.status(201).json({ message: 'Datos del sensor insertados correctamente', insertedId: result.insertedId });
-});
-
-// Endpoint para obtener todos los datos del sensor
-app.get('/sensor-data', async (req, res) => {
-    const db = await connectToMongoDB();
-    const sensorData = await db.collection('sensorData').find().toArray();
-    res.json(sensorData);
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
+run().catch(console.dir);
